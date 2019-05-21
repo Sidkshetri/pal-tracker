5f1f561f429c6edf0db8af61a6eacc4e5c8ede8b
Common Training user <CT0t1868@TechMahindra.com>
2019-05-21 14:00:51 +0530

    Second commit

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,14 +1,26 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 @RestController
 public class WelcomeController {
 
+    String message;
+
+
+    public WelcomeController(@Value("${welcome.message}") String passedMessage) {
+        this.message=passedMessage;
+
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+              return message;
     }
 
 }
--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -8,14 +8,13 @@ import io.pivotal.pal.tracker.EnvController;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EnvControllerTest {
+
+
+
     @Test
     public void getEnv() throws Exception {
         EnvController controller = new EnvController(
-            "8675",
-            "12G",
-            "34",
-            "123.sesame.street"
-        );
+            "8675","12G","34","123.sesame.street"    );
 
         Map<String, String> env = controller.getEnv();
 
